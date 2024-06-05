955f8d3007c25d8334a291e13bca9efd30b89b2c
GuiTavs7 <7tavs7@gmail.com>
2024-06-04 22:12:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let frutas = ["Uva", "Banana", "Melão"];
+
+let videoGame = {
+    nome: "Xbox",
+    valor: 3000
+}
+
+let videoGame2 = {
+    nome: "Playstation",
+    valor: 5000
+}
+
+// INSERINDO OBJETOS DENTRO DE ARRRAYS!
+
+let videoGames = [videoGame, videoGame2];
+
+console.log(videoGames);
+
+// ACESSANDO PROPRIEDADES PELO ARRAY
+
+console.log(videoGames[0].nome);
+console.log(videoGames[0].valor);
+
+console.log(videoGames[1].nome);
+console.log(videoGames[1].valor);
